1079eaf6d6b6cfbb2ec6d278f40d36266e5c6597
Natan Valentim <natan.valentim12@gmail.com>
2022-06-20 16:36:49 +0100

    Readme updated; Showing Gregorian equivalency

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,25 @@ import DekatrianDate from '../services/DekatrianDate'
 
 export class Header extends Component {
   render() {
+    const date = new Date();
+
+    const gregorianDate = () => {
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+
+      return `${day < 10 ? '0' + day : day}/${month < 10 ? '0' + month : month}/${year}`;
+    }
+
     return (
-      <h3>📆
-        {DekatrianDate().date} {DekatrianDate().month} {DekatrianDate().year}
-      </h3>
+      <>
+        <h3>📆
+          {DekatrianDate().date} {DekatrianDate().month} {DekatrianDate().year}
+        </h3>
+        <h4>
+          ({gregorianDate()})
+        </h4>
+      </>
     )
   }
 }
